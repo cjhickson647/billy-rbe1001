580a3df7c580385ddc81e915548418dc47a3368d
cjhickson647 <catakanggames@gmail.com>
2026-05-02 00:25:41 -0400

    ROBERT

--- src/billy_not_jank.py
+++ src/billy_not_jank.py
@@ -14,7 +14,8 @@ controller_1 = Controller(PRIMARY)
 fruit__apricot = Colordesc(1, 233, 77, 81, 10, 0.19)
 fruit__lime = Colordesc(2, 50, 234, 125, 8, 0.28)
 fruit__grape = Colordesc(3, 146, 89, 195, 10, 0.2)
-fruit_vision = AiVision(Ports.PORT16, fruit__apricot, fruit__lime, fruit__grape)
+fruit__tree = Colordesc(4, 121, 163, 160, 40, 0.1)
+fruit_vision = AiVision(Ports.PORT16, fruit__apricot, fruit__lime, fruit__grape, fruit__tree)
 april_vision = AiVision(Ports.PORT14, AiVision.ALL_TAGS)
 imu = Inertial(Ports.PORT6)
 claw = Motor(Ports.PORT19, GearSetting.RATIO_18_1, False)
@@ -28,6 +29,7 @@ right_motor_2 = Motor(Ports.PORT5, GearSetting.RATIO_18_1, True)
 button = Bumper(brain.three_wire_port.a)
 line_tracker_left = Line(brain.three_wire_port.h)
 line_tracker_right = Line(brain.three_wire_port.f)
+ultrasonic = Sonar(brain.three_wire_port.c)
 
 
 # wait for rotation sensor to fully initialize
@@ -97,8 +99,12 @@ RAMP_DRIVE = 1
 SEARCHING = 2
 APPROACHING = 3
 HARVESTING = 4
-DELIVERING = 5
-DEPOSIT_RESET = 6
+AVOID_DANGER = 5
+FIND_WALL = 6
+PANIC = 7
+DELIVERING = 8
+FIND_LINE = 9
+DEPOSIT_RESET = 10
 DOING_YA_MOM = 67
 
 ROBOT_STATE = IDLE
@@ -316,9 +322,20 @@ def detectFruit():
     else:
         return 0, 0
 
-    
-TEST = 0
+def detectTree():
+    object1 = fruit_vision.take_snapshot(fruit__tree)
+    distance = FRUIT_HEIGHT / (object1[0].height * CAMERA_RATIO)
+    xDistance = (object1[0].centerX - 160) * distance * CAMERA_RATIO
+    desiredAngle = math.tan(xDistance / distance) * (180 / math.pi)
+    if object1[0].score > 80:
+        return desiredAngle, distance
+    else:
+        return 0,0
+
+ROBERT = 0
 fruit_count = 0
+distance_values = []
+angle_values = []
 def mission():
     global ROBOT_STATE
     global LAST_STATE
@@ -326,8 +343,13 @@ def mission():
     global drive_task2
     global turn_task
     global fruit_count
-    global TEST
+    global ROBERT
+    global distance_values
+    global angle_values
+    global heading
+    heading = 67
     distance = -67
+    desiredAngle = 670
 
     if ROBOT_STATE == IDLE and controller_1.buttonL1.pressing():
         ROBOT_STATE = RAMP_DRIVE
@@ -341,7 +363,7 @@ def mission():
         else: 
             drive_task.update()
     elif ROBOT_STATE == SEARCHING:
-        if TEST == 0:
+        if ROBERT == 0:
             desiredInfo = detectFruit()
             if desiredInfo == (0, 0):
                 left_motor_1.set_velocity(50)
@@ -354,9 +376,9 @@ def mission():
                 desiredInfo = detectFruit()
                 distance = desiredInfo[1]
                 turn_task = PIDTurn(desiredInfo[0], 2500)
-                TEST = 1
+                ROBERT = 1
 
-        if TEST == 1 and not turn_task.completed:
+        if ROBERT == 1 and not turn_task.completed:
             turn_task.update()
         else:
             ROBOT_STATE = APPROACHING
@@ -373,23 +395,109 @@ def mission():
         else: 
             drive_task.update()
     elif ROBOT_STATE == HARVESTING:
-        if TEST == 1:
+        if ROBERT == 1:
             drive_task = PIDDrive(-5)
-            TEST = 2
-        if TEST == 2:
+            ROBERT = 2
+        if ROBERT == 2:
             claw.spin(FORWARD)
             if timer.time() > 2500:
                 drive_task.update()
             elif drive_task.completed:
                 claw.spin_for(REVERSE, 0.2, SECONDS)
                 fruit_count += 1
-                TEST = 0
                 if fruit_count == 4:
                     ROBOT_STATE = DELIVERING
+                    ROBERT = 3
                 else:
                     ROBOT_STATE = SEARCHING
-    elif ROBOT_STATE == DELIVERING:
-        pass
+                    ROBERT = 0
+    elif ROBOT_STATE == AVOID_DANGER:
+        if ROBERT == 3:
+            drive_task = PIDDrive(-10.24)
+            ROBERT = 4
+        if ROBERT == 4:
+            drive_task.update()
+        elif drive_task.completed:
+            # global heading
+            ROBOT_STATE = FIND_WALL
+            ROBERT = 5
+            timer.reset()
+            heading = imu.rotation(DEGREES)
+            
+
+    elif ROBOT_STATE == FIND_WALL:
+        if ROBERT == 5:
+            right_motor_1.set_velocity(50)
+            right_motor_2.set_velocity(50)
+            left_motor_1.spin(FORWARD)
+            left_motor_2.spin(FORWARD)
+            right_motor_1.spin(REVERSE)
+            right_motor_2.spin(REVERSE)
+            if timer.time() > 500:
+                distance_values.append(ultrasonic.distance(MM)*10)
+                angle_values.append(imu.rotation(DEGREES))
+            if imu.rotation(DEGREES) == heading + 360:
+                ROBERT = 6
+        if ROBERT == 6:
+            distance_values.sort()
+            angle_values.sort()
+            desiredDistance = distance_values[0]
+            desiredAngle = angle_values[0]
+            turn_task = PIDTurn(desiredAngle + 180, 2500)
+            ROBERT = 7
+        if ROBERT == 7:
+            turn_task.update()
+            if turn_task.completed:
+                ROBERT = 8
+                drive_task = PIDDrive(desiredDistance)
+        if ROBERT == 8:
+            info = detectTree()
+            if info[0] == 0 and info[1] == 0:
+                drive_task.update()
+                if drive_task.completed:
+                    ROBERT = 11
+                    ROBOT_STATE = FIND_LINE
+            else:
+                ROBOT_STATE = PANIC
+                desiredAngle = info[0]
+                distance = info[1]
+                ROBERT = 9
+    elif ROBOT_STATE == PANIC:
+        if ROBERT == 9:
+            turn_task = PIDTurn(desiredAngle, 2500)
+            ROBERT = 10
+        if ROBERT == 10:
+            turn_task.update()
+            if turn_task.completed:
+                ROBOT_STATE = FIND_WALL
+                ROBERT = 5
+                timer.reset()
+                heading = imu.rotation(DEGREES)
+    elif ROBOT_STATE == FIND_LINE:
+        if ROBERT == 11:
+            drive_task = PIDDrive(67)
+            ROBERT = 12
+        if ROBERT == 12:
+            drive_task.update()
+            if button.pressed:
+                ROBERT = 13
+        if ROBERT == 13:
+            drive_task = PIDDrive(-6.7)
+            ROBERT = 14
+        if ROBERT == 14:
+            drive_task.update()
+            if drive_task.completed:
+                ROBERT = 15
+        if ROBERT == 15:
+            currentHeading = imu.rotation(DEGREES)
+            turn_task = PIDTurn(currentHeading + 90, 2500)
+            ROBERT = 16
+        if ROBERT == 16:
+            turn_task.update()
+            if turn_task.completed:
+                ROBERT = 17
+        if ROBERT = 17:
+            pass
     elif ROBOT_STATE == DEPOSIT_RESET:
         pass
     else:
